c4565ca5f68772fb59b41fe576b178b330045691
Scott Barr <scottjbarr@gmail.com>
2021-11-18 15:08:48 +1000

    Add "broadcast" command to woc-client binary

--- cmd/woc-client/main.go
+++ cmd/woc-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"os"
@@ -39,6 +40,15 @@ func execute(ctx context.Context, cmd string, args []string) error {
 		hashes := strings.Split(args[0], ",")
 
 		return bulkTXs(ctx, c, hashes)
+
+	case "broadcast":
+		if len(args) == 0 {
+			return errors.New("Usage: woc-client broadcast txhex")
+		}
+
+		data := args[0]
+
+		return broadcast(ctx, c, data)
 	}
 
 	return errors.New("Unknown command")
@@ -52,6 +62,19 @@ func bulkTXs(ctx context.Context, c *woc.Client, hashes []string) error {
 
 	for _, tx := range txs {
 		fmt.Printf("%+v\n", tx)
+	}
+
+	return nil
+}
+
+func broadcast(ctx context.Context, c *woc.Client, data string) error {
+	b, err := hex.DecodeString(data)
+	if err != nil {
+		return err
+	}
+
+	if err := c.Broadcast(ctx, b); err != nil {
+		return err
 	}
 
 	return nil
